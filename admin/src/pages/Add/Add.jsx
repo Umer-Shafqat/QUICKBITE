fccd95e063a6a232dfe074d88c51e27807f6b04a
M.Umer Shafqat <umershafqat251786@gmail.com>
2026-07-16 23:13:32 +0500

    Change API URL to production endpoint

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 function Add() {
-   const url = "http://localhost:4000"; 
+   const url = "https://quickbite-backened.onrender.com"; 
 
   const [image, setImage] = useState(false);
 
